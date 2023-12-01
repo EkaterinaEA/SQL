12c1bdbd98c35e49450a7e6afd44fa41ad2fd7aa
Ekaterina <37078959+EkaterinaEA@users.noreply.github.com>
2023-12-01 13:03:33 -0500

    Add files via upload

---
+++ CREATE_Database.sql
@@ -0,0 +1,11 @@
+-- Database: postgres
+
+-- CREATE Database:
+CREATE DATABASE DatabaseName;
+
+CREATE DATABASE testDB;
+
+-- we can check it in the list of databases as follows:
+SELECT * FROM SYS.DATABASES;
+USE testDB; -- Database changed
+
