e1a7ebf546cc9b4a898bd5844c7db24fbe1564f0
Ekaterina <37078959+EkaterinaEA@users.noreply.github.com>
2023-12-01 13:02:50 -0500

    Add files via upload

---
+++ Expressions.sql
@@ -0,0 +1,9 @@
+-- Database: postgres
+
+-- Syntax
+SELECT column1, column2, columnN
+FROM table_name
+WHERE [CONDITION|EXPRESSION];
+
+-- Select all records from the CUSTOMERS table in which SALARY is equal to 10000
+SELECT * FROM CUSTOMERS WHERE SALARY = 10000;
